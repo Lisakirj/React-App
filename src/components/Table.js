4b1b88c116d945268bbdc5ef297b5d92c5f2fe89
lisa_kirj <lisakirjanova097@gmail.com>
2022-01-31 15:34:38 +0200

    Initial commit

---
+++ src/components/Table.js
@@ -0,0 +1,48 @@
+//Custom class component
+import React, { Component } from "react";
+
+//Simple components
+//1.
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Remove</th>
+      </tr>
+    </thead>
+  );
+};
+//2.
+const TableBody = (props) => {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
+    );
+  });
+
+  return <tbody>{rows}</tbody>;
+};
+//3.
+const Table = (props) => {
+  const { characterData, removeCharacter } = props;
+
+  return (
+    <table>
+      <TableHeader />
+      <TableBody
+        characterData={characterData}
+        removeCharacter={removeCharacter}
+      />
+    </table>
+  );
+};
+
+export default Table;
